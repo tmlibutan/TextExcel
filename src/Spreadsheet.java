e22eae86c637922e92c396b661ec54d658f8b7fa
Marcus Trey <xWolf13@users.noreply.github.com>
2015-02-27 09:31:48 -0800

    FIXED. I hate my life. Handles errors in the cells <3

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,25 +1,44 @@
 public class Spreadsheet {
-	
-	
-	private int rows = 11;
+
 	private int columns = 8;
-	
-	private String[] alphabet = new String[] { "A", "B", "C", "D", "E", "F", "G" };
+	private int rows = 11;
 	private Cell[][] sheet = new Cell[rows][columns];
+	private char[] alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+
+	public Spreadsheet() {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				this.sheet[i][j] = new Cell();
+			}
+		}
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (i == 0 || j == 0) {
+					this.sheet[i][j].setPosition("null");
+				} else {
+					String pos = alphabet[j - 1] + "";
+					pos = pos + i + "";
+					this.sheet[i][j].setPosition(pos);
+				}
+			}
+		}
+		for (int i = 0; i < rows; i++) {
+			if (i == 0) {
+				for (int j = 1; j < columns; j++) {
+					this.sheet[i][j].setValue(alphabet[j - 1] + "");
+				}
+			} else {
+				this.sheet[i][0].setValue(i + "");
+			}
+		}
+	}
 
 	public String printSheet() {
 		String returnString = "";
 
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
-				String cellString = "";
-				for (int k = 1; k <= ((12 - sheet[i][j].toString().length()) / 2); k++) {
-					cellString += " ";
-				}
-				cellString += sheet[i][j].toString();
-				while (cellString.length() < 12) {
-					cellString += " ";
-				}
+				String cellString = this.sheet[i][j].toShort();
 				cellString += "|";
 				returnString += cellString;
 			}
@@ -31,21 +50,28 @@ public class Spreadsheet {
 		return returnString;
 	}
 
-	public Spreadsheet() {
+	public void setValueAt(String label, String input) {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
-				this.sheet[i][j] = new Cell();
-			}
-		}
-		for (int i = 0; i < rows; i++) {
-			if (i == 0) {
-				for (int j = 1; j < columns; j++) {
-					this.sheet[i][j].setValue(alphabet[j - 1]);
+				if (this.sheet[i][j].getPosition().equals(label)) {
+					this.sheet[i][j].setValue(input);
 				}
-			} else {
-				this.sheet[i][0].setValue(i + "");
 			}
 		}
 	}
-}
 
+	public String getValueAt(String str) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (this.sheet[i][j].getPosition().equals(str)) {
+					if (this.sheet[i][j].toFull() != null) {
+						return this.sheet[i][j].toFull();
+					} else {
+						return "<empty>";
+					}
+				}
+			}
+		}
+		return "<ERROR: NO CELL " + str + ">";
+	}
+}
