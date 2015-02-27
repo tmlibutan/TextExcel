bb59ae04cb5c81f678fe3cab65c1caf1c356bba5
Marcus Trey <xWolf13@users.noreply.github.com>
2015-02-27 09:31:16 -0800

    shortValue changes

--- src/Cell.java
+++ src/Cell.java
@@ -1,16 +1,44 @@
-
 public class Cell {
 
-	private String value;
-	
-	public Cell(){
-		this.value = "";
+	private String shortValue;
+	private String fullValue;
+	private String pos;
+
+	public Cell() {
+		this.shortValue = "            ";
 	}
-	
+
+	public String toFull() {
+		return this.fullValue;
+	}
+
+	public String toShort() {
+		return this.shortValue;
+	}
+
 	public void setValue(String str) {
-		this.value = str;
+		this.fullValue = str;
+
+		if (str.length() > 12) {
+			str = str.substring(0, 11) + ">";
+		}
+
+		this.shortValue = "";
+		for (int k = 1; k <= ((12 - str.length()) / 2); k++) {
+			this.shortValue += " ";
+		}
+		this.shortValue += str;
+		while (this.shortValue.length() < 12) {
+			this.shortValue += " ";
+		}
 	}
-	public String toString() {
-		return this.value;
+
+	public String getPosition() {
+		return this.pos;
 	}
+
+	public void setPosition(String str) {
+		this.pos = str;
+	}
+
 }
