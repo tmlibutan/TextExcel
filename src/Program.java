eb342124d7a4686b1f4e6721ec388c7ef7ccf3e3
Marcus Trey <xWolf13@users.noreply.github.com>
2015-03-02 07:57:54 -0800

    Now clears the spreadsheet

--- src/Program.java
+++ src/Program.java
@@ -8,19 +8,22 @@ public class Program {
 	// AP Computer Science
 	// Project 2 - TextExcel
 	// Extra credit so far:
-	// * none so far :
+	// * none so far
 
-	public static void main(String[] args) { // handles user input
+	public static void main(String[] args) { // User input
 		System.out.println("Welcome to TextExcel!");
 		boolean start = true;
 		Spreadsheet sheet = new Spreadsheet();
 		String inputString = "";
 
-		while (start == true) {
+		while (start == true) { // Loops command until "exit"
 			System.out.println("Command/> ");
 			inputString = console.nextLine();
 
-			if (inputString.equals("exit")) {
+			if (inputString.equals("clear")) {
+				Spreadsheet clear = new Spreadsheet();
+				sheet = clear;
+			} else if (inputString.equals("exit")) {
 				start = false;
 				System.out.println("Farewell! See you in the next milestone ^_^");
 			} else if (inputString.equalsIgnoreCase("print")) {
@@ -28,23 +31,15 @@ public class Program {
 				System.out.println(sheet.printSheet());
 			} else if (inputString.indexOf('=') != -1) {
 				if (inputString.toLowerCase().contains("null")) {
-					System.out.println("<ERROR: NO CELL " + inputString.substring(0, inputString.indexOf('=') - 1) + ">");
+					System.out.println("<ERROR: NO CELL " + inputString.substring(0, inputString.indexOf('=') - 1)
+							+ ">");
 				} else {
 					String cellPos = inputString.substring(0, inputString.indexOf('=') - 1);
 					inputString = inputString.substring(inputString.indexOf("=") + 2, inputString.length());
 					sheet.setValueAt(cellPos, inputString);
 				}
-			} else {
-				System.out.println();
-				if (inputString.toLowerCase().equalsIgnoreCase("null")) {
-					System.out.println("<ERROR: NO CELL " + inputString + ">");
-				} else if (!sheet.getValueAt(inputString).contains("ERROR")) {
-					System.out.println(inputString + " = " + sheet.getValueAt(inputString));
-				} else {
-					System.out.println(sheet.getValueAt(inputString));
-				}
-				System.out.println();
 			}
 		}
 	}
 }
+
