463c07011ab83e8bbb95b80e50ad4f01c053d4c6
Marcus Trey <xWolf13@users.noreply.github.com>
2015-03-11 07:49:17 -0700

    JavaDoc comments yipee

--- src/Program.java
+++ src/Program.java
@@ -3,10 +3,9 @@ import java.util.*;
 public class Program {
 
 	static Scanner console = new Scanner(System.in);
-
-	// Marcus Libutan
-	// AP Computer Science
-	// Project 2 - TextExcel
+	 // Marcus Libutan
+	 // AP Computer Science Project 2 - TextExcel
+	 // Extra credit
 	// Extra credit so far:
 	// * none so far
 
@@ -16,23 +15,43 @@ public class Program {
 		Spreadsheet sheet = new Spreadsheet();
 		String inputString = "";
 
-		while (start == true) { // Loops command until "exit"
+		/**
+		 * Below this, the program will get input and determine if the program
+		 * is started or terminated. This also handles the clear, exit, and
+		 * print commands.
+		 */
+		while (start == true) {
+			/**
+			 * while 'start' is true, the program stays running. Once exit is
+			 * entered this will end/terminate the program
+			 */
 			System.out.println("Command/> ");
 			inputString = console.nextLine();
 
 			if (inputString.equals("clear")) {
 				Spreadsheet clear = new Spreadsheet();
 				sheet = clear;
+				/**
+				 * New Spreadsheet "clear" is created completely making a new
+				 * spreadsheet
+				 */
 			} else if (inputString.equals("exit")) {
 				start = false;
+				/**
+				 * boolean start changes from true to false. This will terminate
+				 * the program rather than using System.exit(0);
+				 */
 				System.out.println("Farewell! See you in the next milestone ^_^");
 			} else if (inputString.equalsIgnoreCase("print")) {
 				System.out.println();
 				System.out.println(sheet.printSheet());
+				/**
+				 * print will call to the spreadsheet class and create a new
+				 * spreadsheet according to the values of printsheet()
+				 */
 			} else if (inputString.indexOf('=') != -1) {
 				if (inputString.toLowerCase().contains("null")) {
-					System.out.println("<ERROR: NO CELL " + inputString.substring(0, inputString.indexOf('=') - 1)
-							+ ">");
+					System.out.println("Error_NoCell/>" + inputString.substring(0, inputString.indexOf('=') - 1) + ">");
 				} else {
 					String cellPos = inputString.substring(0, inputString.indexOf('=') - 1);
 					inputString = inputString.substring(inputString.indexOf("=") + 2, inputString.length());
@@ -42,4 +61,3 @@ public class Program {
 		}
 	}
 }
-
