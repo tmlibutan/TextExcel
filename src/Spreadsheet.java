9eb19b9d3daec471db1bc4052287e461c4fd4035
Marcus Trey <xWolf13@users.noreply.github.com>
2015-04-03 01:01:42 -0700

    Fixed Save and Load. Last commit..

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -6,11 +6,13 @@ public class Spreadsheet implements Savable {
 	private int columns = 8;
 	private char[] alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
 	private Cell[][] sheet = new Cell[rows][columns];
+	private String name = "";
 
 	/**
 	 * Above this are the values for the spreadsheet. A-G are the labeled
 	 * X-Axis. We need 11 rows and 8 columns as we have a empty cell on the very
-	 * first cell. Used a two-dimensional array to represent the row and columns.
+	 * first cell. Used a two-dimensional array to represent the row and
+	 * columns.
 	 */
 	public String printSheet() {
 		/**
@@ -62,20 +64,66 @@ public class Spreadsheet implements Savable {
 		}
 	}
 
+	public void clearCellAt(String str) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (sheet[i][j].getPos().equals(str)) {
+					String pos = sheet[i][j].getPos();
+					sheet[i][j] = new Cell();
+					sheet[i][j].setPosition(pos);
+				}
+			}
+		}
+	}
+
+	public Cell[][] getArray() {
+		return this.sheet;
+	}
+
+	public void setEquals(Cell[][] sheet) {
+		for (int i = 1; i < rows; i++) {
+			for (int j = 1; j < columns; j++) {
+				sheet[i][j].setValue(sheet[i][j].toFull());
+			}
+		}
+	}
+
+	public void setName(String str) {
+		name = str;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 	public void setValueAt(String label, String input) {
 		/**
 		 * This method handles where the input should be put in (e.g A10). This
 		 * calls to getPosition to find out where the cell is and then puts in
-		 * the input in that cell. 
+		 * the input in that cell.
 		 */
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
-				if (this.sheet[i][j].getPosition().equals(label)) {
+				if (this.sheet[i][j].getPos().equals(label)) {
 					this.sheet[i][j].setValue(input);
 				}
 			}
 		}
+	}
 
+	public String getValueAt(String str) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (sheet[i][j].getPos().equals(str.toUpperCase())) {
+					if (sheet[i][j].toFull() != "            ") {
+						return sheet[i][j].toFull();
+					} else {
+						return "<empty>";
+					}
+				}
+			}
+		}
+		return "Error:/>" + str + ">";
 	}
 
 	/**
@@ -83,14 +131,30 @@ public class Spreadsheet implements Savable {
 	 * in the root of the TextExcel folder. This will also save a file to the
 	 * same location
 	 */
+
 	@Override
 	public String[] getSaveData() {
+		String[] values = new String[columns * rows];
 
-		return null;
+		for (int i = 1; i < rows; i++) {
+			for (int j = 1; j < columns; j++) {
+				values[j * (i + 1)] = sheet[i][j].toFull();
+			}
+		}
+
+		return values;
 	}
 
 	@Override
 	public void loadFrom(String[] saveData) {
-
+		for (int i = 1; i < rows; i++) {
+			for (int j = 1; j < columns; j++) {
+				if (saveData[i * (j + 1)].charAt(0) == '\"'
+						&& saveData[i * (j + 1)].charAt(saveData[i * (j + 1)].length() - 1) == '\"') {
+					sheet[j][i] = new StringCell(saveData[i * (j + 1)], saveData[i * (j + 1)].substring(1, saveData[i
+							* (j + 1)].length() - 1), sheet[j][i].getPos());
+				}
+			}
+		}
 	}
 }
